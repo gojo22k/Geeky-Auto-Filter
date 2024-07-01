b125e208774db49d29ae4d05ec0c403b54d14dcb
Noob Developer <157978295+Geekyowner@users.noreply.github.com>
2024-07-01 18:03:31 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -681,13 +681,13 @@ Also your tutorial will be Added Your Group..."""
     DEVELOPER_TXT = """
 special Thanks To ❤️ Developers -
 
--Dev 1 [Owner of this bot ]<a href='https://t.me/KingVj01'>VJ</a>
+-Dev 1 [Owner of this bot ]<a href='https://t.me/Ig_1Venom'>Venom 😎</a>
 
--Dev 2 <a href='https://t.me/vjbots_bot'>VJ</a>
+-Dev 2 <a href='https://t.me/Geeky_Network'>Main Channel</a>
 
--Dev 3 <a href='https://t.me/vj_botz>VJ Botz</a>
+-Dev 3 <a href='https://t.me/Geeky_Updates>Update Channel</a>
 
-- Dev 4 <a href='https://t.me/vj_bots'>TEAM VJ</a>
+- Dev 4 <a href='https://t.me/Movies_Search_Groupp'>Movie Group</a>
 """
 
     RENAME_TXT = """
