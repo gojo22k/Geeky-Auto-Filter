1aaf623cf76e6c10c3d648df8b0d5a9b4f834482
Noob Developer <157978295+Geekyowner@users.noreply.github.com>
2024-07-18 10:17:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -101,7 +101,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Publicearn.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'd92f120bd61ee18e32d4a5e4f4cfad8d747eeec4')
 
